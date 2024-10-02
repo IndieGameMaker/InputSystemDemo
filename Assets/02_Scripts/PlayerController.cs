ce74190a4c851922a33dc614042b93f1e81d0672
LeeJaeHyun <myleje@gmail.com>
2024-10-02 11:49:24 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionProperty moveAction; // Vector2 (x, y)
     [SerializeField] private InputActionProperty attackAction; // 버튼 bool
+
+    private CharacterController controller;
+    private CinemachineCamera cinemachineCamera;
 
     private Vector2 moveInput;
 
@@ -27,15 +31,29 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
+        cinemachineCamera = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
     }
 
     void Update()
     {
-        Debug.Log($"x={moveInput.x} / y={moveInput.y}");
-        //moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // 씨네머신 카메라의 벡터를 계산
+        Vector3 camForward = cinemachineCamera.transform.forward;
+        Vector3 camRight = cinemachineCamera.transform.right;
+        // 평면 벡터로 변환
+        camForward.y = camRight.y = 0.0f;
+        // 벡터의 정규화 
+        camForward.Normalize(); // camForward = camForward.nomalized;
+        camRight.Normalize();
 
-        //if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return)) Debug.Log("공격");
+        // 이동 벡터 계산
+        // Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        Vector3 moveDir = (camForward * moveInput.y) + (camRight * moveInput.x);
+        moveDir.Normalize();
 
+        // 회전 처리
+        transform.rotation = Quaternion.LookRotation(moveDir);
+        // 이동 처리
+        controller.Move(moveDir * Time.deltaTime * 8.0f);
     }
 }
